ddc723f2cafdb814e36c5ab6cfbf659d925b5256
AnthonyLzq <sluzquinosa@uni.pe>
2022-01-13 01:34:00 -0500

    feat: project initialization and use state hook

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Header/>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
